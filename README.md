f175f3b701d3b82fa46182f51865e7b3fb1d63f3
Mahmudur Rahman <nahidacm@gmail.com>
2023-05-23 08:51:05 +0600

    Campaign traking table migration added

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 1. Clone this repo.
 2. `cd <path to repo>`
 3. `./vendor/bin/sail up -d`
+
+
+## Observation on the requirements
+- What is the point of taking the client IP from the client side. moreover, there is some trust issue on this.
+- I had to change the `date` column to `record_date`. Because using keywords as table column titles sometimes cause unexpected issues.
+- In `PK (date, country_code, campaign_id, creative_id, browser_id, device_id)` did you mean Primary Key? But, a table can't have multiple primary keys.
---
+++ database/migrations/2023_05_23_014340_campaign_tracking.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('campaign_tracking', function (Blueprint $table) {
+            $table->date('record_date');
+            $table->char('country_code', 3);
+            $table->integer('campaign_id');
+            $table->integer('creative_id');
+            $table->integer('browser_id');
+            $table->integer('device_id');
+            $table->bigInteger('count')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop('campaign_tracking');
+    }
+};
